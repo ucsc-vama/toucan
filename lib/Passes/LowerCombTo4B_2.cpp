03460fcdf7a0360189295d53e1d206c46b62fdc3
H Wang <haoyuanwang102@gmail.com>
2024-02-02 11:42:19 -0800

    Modify LowerCombTo4B_2 pass

--- lib/Passes/LowerCombTo4B_2.cpp
+++ lib/Passes/LowerCombTo4B_2.cpp
@@ -1,4 +1,6 @@
 
+#include "circt/Dialect/HW/HWDialect.h"
+#include "circt/Dialect/HW/HWOps.h"
 #include "circt/Dialect/HW/HWTypes.h"
 #include "circt/Support/LLVM.h"
 #include "circt/Dialect/Comb/CombDialect.h"
@@ -11,10 +13,17 @@
 #include "mlir/IR/BuiltinTypes.h"
 #include "mlir/IR/OpImplementation.h"
 #include "mlir/IR/Operation.h"
+#include "mlir/IR/ValueRange.h"
 #include "mlir/IR/Visitors.h"
+#include "mlir/Rewrite/FrozenRewritePatternSet.h"
 #include "mlir/Support/LLVM.h"
 #include "mlir/IR/Threading.h"
 #include "mlir/Support/LogicalResult.h"
+#include "mlir/Transforms/DialectConversion.h"
+#include "mlir/Transforms/GreedyPatternRewriteDriver.h"
+
+#include "toucan/ToucanAttributes.h"
+#include "toucan/ToucanDialect.h"
 #include "toucan/ToucanTypes.h"
 
 #include "llvm/ADT/SmallPtrSet.h"
@@ -23,9 +32,13 @@
 
 #include "llvm/Support/Casting.h"
 #include "llvm/Support/Debug.h"
+#include "llvm/Support/ErrorHandling.h"
+#include "llvm/Support/MathExtras.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/Support/Format.h"
 
 #include <memory>
+#include <string>
 
 
 #define GEN_PASS_DEF_LOWERCOMBTO4B_2
@@ -41,24 +54,78 @@ using namespace llvm;
 
 #define DEBUG_TYPE "LowerCombTo4B_2Pass"
 
+
+
+struct LowerCombAddOp: OpRewritePattern<comb::AddOp> {
+  using OpRewritePattern<comb::AddOp>::OpRewritePattern;
+
+  LogicalResult matchAndRewrite(comb::AddOp op, PatternRewriter &rewriter) const final {
+    op->emitError("Operation not supported");
+    return failure();
+  }
+};
+
+struct LowerCombSubOp: OpRewritePattern<comb::SubOp> {
+  using OpRewritePattern<comb::SubOp>::OpRewritePattern;
+
+  LogicalResult matchAndRewrite(comb::SubOp op, PatternRewriter &rewriter) const final {
+    op->emitError("Operation not supported");
+    return failure();
+  }
+};
+
+struct LowerCombMuxOp: OpRewritePattern<comb::MuxOp> {
+  using OpRewritePattern<comb::MuxOp>::OpRewritePattern;
+
+  LogicalResult matchAndRewrite(comb::MuxOp op, PatternRewriter &rewriter) const final {
+    op->emitError("Operation not supported");
+    return failure();
+  }
+};
+
+
+
+
+
+
 struct LowerCombTo4B_2Pass : toucan::impl::LowerCombTo4B_2Base<LowerCombTo4B_2Pass> {
   using LowerCombTo4B_2Base<LowerCombTo4B_2Pass>::LowerCombTo4B_2Base;
+
+  std::shared_ptr<FrozenRewritePatternSet> patterns;
+  std::shared_ptr<ConversionTarget> target;
+
+  LogicalResult initialize(MLIRContext *context) override {
+
+    RewritePatternSet owningPatterns(context);
+    ConversionTarget conversionTarget(*context);
+    
+    owningPatterns.add<LowerCombAddOp>(context);
+    owningPatterns.add<LowerCombSubOp>(context);
+    owningPatterns.add<LowerCombMuxOp>(context);
+
+
+    conversionTarget.addLegalDialect<toucan::ToucanDialect>();
+    conversionTarget.addLegalDialect<hw::HWDialect>();
+    conversionTarget.addLegalDialect<comb::CombDialect>();
+
+    // After lowering, following ops should no longer appear
+    conversionTarget.addIllegalOp<comb::AddOp>();
+    conversionTarget.addIllegalOp<comb::SubOp>();
+    conversionTarget.addIllegalOp<comb::MuxOp>();
+
+    patterns = std::make_shared<FrozenRewritePatternSet>(std::move(owningPatterns));
+    target = std::make_shared<ConversionTarget>(std::move(
+    conversionTarget));
+
+    return success();
+  }
+
 
   LogicalResult runOnModule(hw::HWModuleOp mod) {
     SmallVector<Operation*> toRemove;
 
-    // TODO: Lower add
-    // TODO: lower mux
-    // TODO: lower sub
-    // TODO: 
+    return applyFullConversion(mod, *target, *patterns);
 
-    // for (auto &stmt: mod.getOps()) {
-
-    // }
-
-    for (auto op: toRemove) op->erase();
-
-    return success();
   }
 
   void runOnOperation() final {
