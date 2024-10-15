ba9926620453ba97578ad343032a08503bca264e
H Wang <haoyuanwang102@gmail.com>
2024-10-15 16:52:49 -0700

    Rewrite for better pass speed

--- lib/Passes/DeduplicateRegisters.cpp
+++ lib/Passes/DeduplicateRegisters.cpp
@@ -48,90 +48,68 @@ using namespace llvm;
 // TODO: dump merged register list for future use (waveform, for example)
 
 
-std::atomic<uint64_t> dedup_reg_cnt = 0;
-
-struct DedupRegs: OpRewritePattern<toucan::RegWriteOp> {
-  using OpRewritePattern<toucan::RegWriteOp>::OpRewritePattern;
-
-
-  LogicalResult matchAndRewrite(toucan::RegWriteOp op, PatternRewriter &rewriter) const final {
-
-    auto regWriteDataVal = op.getData();
-    auto regVal = op.getReg();
-
-    mlir::SmallVector<toucan::RegWriteOp> sameRegWriteOps;
-    for (auto eachUser: regWriteDataVal.getUsers()) {
-      if (auto eachRegWrite = dyn_cast<toucan::RegWriteOp>(eachUser)) {
-        sameRegWriteOps.push_back(eachRegWrite);
-      }
-    }
-
-    assert(sameRegWriteOps.size() >= 1);
-    if (sameRegWriteOps.size() == 1) {
-      // No duplicated regs, no need to dedup
-      return failure();
-    }
-
-    assert(llvm::find(sameRegWriteOps, op) != sameRegWriteOps.end());
-
-    // auto regValNamehint = getSVNameHintAttr(op);
-
-    uint64_t dedupCnt = 0;
-
-    for (auto eachRegWrite: sameRegWriteOps) {
-      auto regValToMerge = eachRegWrite.getReg();
-      
-      if (eachRegWrite != op) {
-        // Merge this reg with current op reg
-        assert(regValToMerge != regVal);
-
-
-        // auto toMergeRegValNamehint = getSVNameHintAttr(eachRegWrite);
-        // LLVM_DEBUG(llvm::dbgs() << "Merging " << regValNamehint << " with " << toMergeRegValNamehint << "\n");
-
-        regValToMerge.replaceAllUsesWith(regVal);
-        rewriter.eraseOp(eachRegWrite);
-
-        dedupCnt++;
-      }
-    }
-
-    if (dedupCnt > 0) {
-      dedup_reg_cnt += dedupCnt;
-      return success();
-    }
-
-    return failure();
-  }
-};
-
-
-
-
 struct DeduplicateRegisters : toucan::impl::DeduplicateRegistersBase<DeduplicateRegisters> {
   using DeduplicateRegistersBase<DeduplicateRegisters>::DeduplicateRegistersBase;
 
-  std::shared_ptr<FrozenRewritePatternSet> patterns;
-  // std::shared_ptr<ConversionTarget> target;
 
-  LogicalResult initialize(MLIRContext *context) override {
-    RewritePatternSet owningPatterns(context);
-    // ConversionTarget conversionTarget(*context);
-    
-    owningPatterns.add<DedupRegs>(context);
-
-    patterns = std::make_shared<FrozenRewritePatternSet>(std::move(owningPatterns));
-
-    return success();
-  }
 
   void runOnOperation() final {
-    auto mod = getOperation();
 
-    auto ret = applyPatternsAndFoldGreedily(mod, *patterns);
-    if (failed(ret)) signalPassFailure();
+    mlir::DenseSet<Operation*> toRemove;
+    uint64_t dedupCnt = 0;
 
-    LLVM_DEBUG(llvm::dbgs() << "Dedup " << dedup_reg_cnt << " registers\n");
+
+    getOperation()->walk([&](toucan::RegWriteOp op){
+      if (toRemove.contains(op)) return;
+
+      auto regWriteDataVal = op.getData();
+      auto regVal = op.getReg();
+
+      mlir::SmallVector<toucan::RegWriteOp> sameRegWriteOps;
+      for (auto eachUser: regWriteDataVal.getUsers()) {
+        if (auto eachRegWrite = dyn_cast<toucan::RegWriteOp>(eachUser)) {
+          sameRegWriteOps.push_back(eachRegWrite);
+        }
+      }
+
+      assert(sameRegWriteOps.size() >= 1);
+      if (sameRegWriteOps.size() == 1) {
+        // No duplicated regs, no need to dedup
+        return;
+      }
+
+      assert(llvm::find(sameRegWriteOps, op) != sameRegWriteOps.end());
+
+      // auto regValNamehint = getSVNameHintAttr(op);
+
+      for (auto eachRegWrite: sameRegWriteOps) {
+        auto regValToMerge = eachRegWrite.getReg();
+        
+        if (eachRegWrite != op) {
+          // Merge this reg with current op reg
+          assert(regValToMerge != regVal);
+
+          auto regValDefOp = regValToMerge.getDefiningOp();
+
+
+          // auto toMergeRegValNamehint = getSVNameHintAttr(eachRegWrite);
+          // LLVM_DEBUG(llvm::dbgs() << "Merging " << regValNamehint << " with " << toMergeRegValNamehint << "\n");
+
+          regValToMerge.replaceAllUsesWith(regVal);
+
+          toRemove.insert(eachRegWrite);
+          toRemove.insert(regValDefOp);
+
+          dedupCnt++;
+        }
+      }
+    });
+
+    for (auto op: toRemove) {
+      op->erase();
+    }
+
+    LLVM_DEBUG(llvm::dbgs() << "Dedup " << dedupCnt << " registers\n");
   }
 
 };
