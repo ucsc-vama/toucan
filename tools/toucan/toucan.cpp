bf3b81e00ce974ee6dc3dbf3f2c05b5822ec592e
H Wang <haoyuanwang102@gmail.com>
2025-07-28 13:04:01 -0700

    Add parameter to remove all print and assertion

--- tools/toucan/toucan.cpp
+++ tools/toucan/toucan.cpp
@@ -63,6 +63,7 @@ static cl::opt<bool> verbose("v", cl::desc("verbose"), cl::init(false), cl::cat(
 static cl::opt<bool> gpuCodeGen("gpu", cl::desc("Generate GPU code"), cl::init(false), cl::cat(mainCategory));
 // static cl::opt<bool> cpuCodeGen("cpu", cl::desc("Generate CPU code (single thread)"), cl::init(false), cl::cat(mainCategory));
 static cl::opt<bool> dumpOutputMLIR("dump", cl::desc("Dump output.mlir"), cl::init(false), cl::cat(mainCategory));
+static cl::opt<bool> removeSVDialect("removeSVDialect", cl::desc("Remove print and assertion"), cl::init(false), cl::cat(mainCategory));
 static cl::opt<uint32_t> targetGPUSMs("gpuSMs", cl::desc("Target GPU SM count. This argument affects number of partitions if partSize is not explicitely given."), cl::init(142), cl::cat(mainCategory));
 // static cl::opt<uint32_t> partitionNumRegions("numRegions", cl::desc("Partitioning region count."), cl::init(4), cl::cat(mainCategory));
 static cl::opt<float> partitionIbFactor("ibFactor", cl::desc("Partitioning target imbalance factor (1.0 > ibFactor > 0.0)."), cl::init(0.015), cl::cat(mainCategory));
@@ -91,9 +92,11 @@ static LogicalResult compileAndEmit(
 
     if (inputLevel < ToucanHigh) {
         // Lower to ToucanHigh
-        
-        // Expand SV macros, generates Print and Stop
-        pm.addPass(toucan::createExpandSVMacroPass());
+
+        if (!removeSVDialect) {
+            // Expand SV macros, generates Print and Stop
+            pm.addPass(toucan::createExpandSVMacroPass());
+        }
         // Remove unsupported Ops (other SV Ops, OM Ops)
         pm.addPass(toucan::createRemoveSVnOMPass());
         // SplitRWPort
